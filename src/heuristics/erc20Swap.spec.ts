6966a0774fbf56bff396c2a31004156dc49e1c88
Paul Cowgill <pauldavidcowgill@gmail.com>
2023-11-20 17:38:53 -0600

    Sketch out repo structure

---
+++ src/heuristics/erc20Swap.spec.ts
@@ -0,0 +1,18 @@
+import { detectERC20Swap } from './erc20Swap';
+import erc20Swap0x8cb66698 from '../test/transactions/erc20Swap-0x8cb66698.json';
+import erc20SwapNot0xb376ca2f from '../test/transactions/erc20Swap-not-0xb376ca2f.json';
+import { Transaction } from '../types/transaction';
+
+describe('ERC20 Swap', () => {
+  it('Should detect ERC20 Swap transaction', () => {
+    const isERC20Swap = detectERC20Swap(erc20Swap0x8cb66698 as Transaction);
+
+    expect(isERC20Swap).toBe(true);
+  });
+
+  it('Should not detect as ERC20 Swap transaction', () => {
+    const isERC20Swap = detectERC20Swap(erc20SwapNot0xb376ca2f as Transaction);
+
+    expect(isERC20Swap).toBe(false);
+  });
+});
