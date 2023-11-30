735f24764d4658156561e570b077ffb1d7a1dcf9
ponyjackal <pony1125@outlook.com>
2023-11-30 05:18:00 -0800

    feat: create contextualizer using npm script

--- src/commands/program.ts
+++
@@ -1,9 +0,0 @@
-const { Command } = require('commander');
-const program = new Command();
-
-program
-  .name('Onceupon command')
-  .description('CLI to some contextualizer utils')
-  .version('0.1.0');
-
-export default program;
