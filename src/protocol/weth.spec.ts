6204fefc83ce68ba9d6980e94be2ecff98f1517f
ponyjackal <pony1125@outlook.com>
2023-12-04 05:44:12 -0800

    feat: add failing test for weth

---
+++ src/protocol/weth.spec.ts
@@ -0,0 +1,10 @@
+import { Transaction } from '../types';
+import { detectWeth } from './weth';
+import catchall0xc35c01ac from '../test/transactions/catchall-0xc35c01ac.json';
+
+describe('Weth Faucet', () => {
+  it('Should not detect as weth', () => {
+    const weth1 = detectWeth(catchall0xc35c01ac as Transaction);
+    expect(weth1).toBe(false);
+  });
+});
