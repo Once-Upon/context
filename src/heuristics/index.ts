e685424a9435a00fc05b886a2c70f6d5ee741fca
Paul Cowgill <pauldavidcowgill@gmail.com>
2023-12-04 20:21:02 -0600

    Add a note with rationale for odd exports

--- src/heuristics/index.ts
+++ src/heuristics/index.ts
@@ -1,3 +1,5 @@
+// Note: exporting explicitly from the index file so that exports
+// from elsewhere in the dir won't be exported as well
 export * from './cancelPendingTransaction/index';
 export * from './contractDeployment/index';
 export * from './erc20Swap/index';
--- src/protocol/index.ts
+++ src/protocol/index.ts
@@ -1,3 +1,5 @@
+// Note: exporting explicitly from the index file so that exports
+// from elsewhere in the dir won't be exported as well
 export * from './weth/index';
 export * from './ens/index';
 export * from './superchainFaucet/index';
