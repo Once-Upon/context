276df1eebbd9789db1d5782bb6c555a07b9c5bee
ponyjackal <pony1125@outlook.com>
2023-12-06 09:04:47 -0800

    feat: add unit test for leeroy

---
+++ src/protocol/leeroy/leeroy.spec.ts
@@ -0,0 +1,25 @@
+import { Transaction } from '../../types';
+import { detectLeeroy, generateLeeroyContext } from './leeroy';
+import leeroy0x9c6d7a1a from '../../test/transactions/leeroy-0x9c6d7a1a.json';
+import catchall0xc35c01ac from '../../test/transactions/catchall-0xc35c01ac.json';
+
+describe('Leeroy', () => {
+  it('Should detect leeroy', () => {
+    const leeroy1 = detectLeeroy(leeroy0x9c6d7a1a as Transaction);
+    expect(leeroy1).toBe(true);
+  });
+
+  it('Should generate leeroy context', () => {
+    const leeroy1 = generateLeeroyContext(leeroy0x9c6d7a1a as Transaction);
+    expect(leeroy1.context?.summaries.category).toBe('PROTOCOL_1');
+    expect(leeroy1.context?.summaries.en.title).toBe('Leeroy');
+    expect(leeroy1.context?.summaries.en.variables?.contextAction?.value).toBe(
+      'posted',
+    );
+  });
+
+  it('Should not detect as leeroy', () => {
+    const leeroy1 = detectLeeroy(catchall0xc35c01ac as Transaction);
+    expect(leeroy1).toBe(false);
+  });
+});
