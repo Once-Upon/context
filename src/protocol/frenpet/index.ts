43bf669e9d412b027ae4efa2054362fe208b76e2
ponyjackal <pony1125@outlook.com>
2023-12-08 09:16:35 -0800

    feat: update frenpet

--- src/protocol/frenpet/index.ts
+++ src/protocol/frenpet/index.ts
@@ -1,1 +1,18 @@
-export { frenPetContextualizer } from './gameplay';
+import { Transaction } from '../../types';
+import { contextualize as frenPet } from './gameplay';
+
+const children = { frenPet };
+
+const contextualize = (transaction: Transaction): Transaction => {
+  for (const childContextualizer of Object.values(children)) {
+    const result = childContextualizer(transaction);
+    if (result.context?.summaries?.category) {
+      return result;
+    }
+  }
+};
+
+export const frenPetContextualizer = {
+  contextualize,
+  children,
+};
