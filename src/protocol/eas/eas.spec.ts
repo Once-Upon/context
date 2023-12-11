8335c24b9431f90a782c91dc7b1b20fd3276db9d
Daniel Öhrlund <danielohrlund@gmail.com>
2023-12-11 22:40:15 +0100

    update EAS tests to match against contextSummary instead of variables

--- src/protocol/eas/eas.spec.ts
+++ src/protocol/eas/eas.spec.ts
@@ -1,5 +1,6 @@
 import { Transaction } from '../../types';
 import { detect, generate } from './eas';
+import { contextSummary } from '../../helpers/utils';
 import easAttest0xfed2349f from '../../test/transactions/eas-attest-mainnet-0xfed2349f.json';
 import easAttestByDelegation0xb58d6544 from '../../test/transactions/eas-attest-by-delegation-optimism-0xb58d6544.json';
 import easMultiAttest0x9bab2b2e from '../../test/transactions/eas-multi-attest-linea-0x9bab2b2e.json';
@@ -25,20 +26,11 @@ describe('EAS', () => {
 
     it('Should generate context', () => {
       const transaction = generate(easAttest0xfed2349f as Transaction);
-      expect(transaction.context.summaries.en.variables.attested?.type).toBe(
-        'contextAction',
-      );
 
-      expect(transaction.context.variables).toMatchObject({
-        from: { value: '0x3b60e31cfc48a9074cd5bebb26c9eaa77650a43f' },
-        schema: {
-          value:
-            '0xc59265615401143689cbfe73046a922c975c99d97e4c248070435b1104b2dea7',
-        },
-        recipient: {
-          value: '0x9934465Ee73BeAF148b1b3Ff232C8cD86c4c2c63',
-        },
-      });
+      expect(transaction.context.summaries.en.title).toBe('EAS');
+      expect(contextSummary(transaction.context)).toBe(
+        '0x3b60e31cfc48a9074cd5bebb26c9eaa77650a43f ATTESTED to 0x9934465Ee73BeAF148b1b3Ff232C8cD86c4c2c63 with schema 0xc59265615401143689cbfe73046a922c975c99d97e4c248070435b1104b2dea7',
+      );
     });
   });
 
@@ -52,20 +44,11 @@ describe('EAS', () => {
       const transaction = generate(
         easAttestByDelegation0xb58d6544 as Transaction,
       );
-      expect(transaction.context.summaries.en.variables.attested?.type).toBe(
-        'contextAction',
+
+      expect(transaction.context.summaries.en.title).toBe('EAS');
+      expect(contextSummary(transaction.context)).toBe(
+        '0xc13D679471FEa46193891343EEAF761bFc52808E ATTESTED to 0x917DE4FEc44841312F632D2A020867Fe0c6AeA43 with schema 0xeb2a4b4be5355128b420a8045a47750aab8ba427014401387a564bbed987d16c by delegation via 0x917de4fec44841312f632d2a020867fe0c6aea43',
       );
-      expect(transaction.context.variables).toMatchObject({
-        from: { value: '0x917de4fec44841312f632d2a020867fe0c6aea43' },
-        attester: { value: '0xc13D679471FEa46193891343EEAF761bFc52808E' },
-        schema: {
-          value:
-            '0xeb2a4b4be5355128b420a8045a47750aab8ba427014401387a564bbed987d16c',
-        },
-        recipient: {
-          value: '0x917DE4FEc44841312F632D2A020867Fe0c6AeA43',
-        },
-      });
     });
   });
 
@@ -77,16 +60,10 @@ describe('EAS', () => {
 
     it('Should generate context', () => {
       const transaction = generate(easMultiAttest0x9bab2b2e as Transaction);
-      expect(transaction.context.summaries.en.variables.attested?.type).toBe(
-        'contextAction',
+      expect(transaction.context.summaries.en.title).toBe('EAS');
+      expect(contextSummary(transaction.context)).toBe(
+        '0x0fb166cddf1387c5b63ffa25721299fd7b068f3f ATTESTED 2 times with 1 schemas',
       );
-      expect(transaction.context.variables).toMatchObject({
-        from: { value: '0x0fb166cddf1387c5b63ffa25721299fd7b068f3f' },
-        schemas: { value: '1' },
-        count: {
-          value: '2',
-        },
-      });
     });
   });
 
@@ -98,16 +75,10 @@ describe('EAS', () => {
 
     it('Should generate context', () => {
       const transaction = generate(easRevoke0x4ec6335e as Transaction);
-      expect(transaction.context.summaries.en.variables.revoked?.type).toBe(
-        'contextAction',
+      expect(transaction.context.summaries.en.title).toBe('EAS');
+      expect(contextSummary(transaction.context)).toBe(
+        '0x6e91973dee716ed6859e7bb689c9bd2955bdb96e REVOKED an attestation with schema 0xd3f24e873e8df2d9bb9af6f08ea1ddf61f65754d023f3ea761081e3e6a226a80',
       );
-      expect(transaction.context.variables).toMatchObject({
-        from: { value: '0x6e91973dee716ed6859e7bb689c9bd2955bdb96e' },
-        schema: {
-          value:
-            '0xd3f24e873e8df2d9bb9af6f08ea1ddf61f65754d023f3ea761081e3e6a226a80',
-        },
-      });
     });
   });
 
@@ -119,12 +90,10 @@ describe('EAS', () => {
 
     it('Should generate context', () => {
       const transaction = generate(easTimestamp0x892d0c6e as Transaction);
-      expect(transaction.context.summaries.en.variables.timestamped?.type).toBe(
-        'contextAction',
+      expect(transaction.context.summaries.en.title).toBe('EAS');
+      expect(contextSummary(transaction.context)).toBe(
+        '0x79ffc4cf151373226dcc59c9582395214a364358 TIMESTAMPED data',
       );
-      expect(transaction.context.variables).toMatchObject({
-        from: { value: '0x79ffc4cf151373226dcc59c9582395214a364358' },
-      });
     });
   });
 
@@ -136,12 +105,10 @@ describe('EAS', () => {
 
     it('Should generate context', () => {
       const transaction = generate(easRevokeOffchain0xf38b96d0 as Transaction);
-      expect(transaction.context.summaries.en.variables.revoked?.type).toBe(
-        'contextAction',
+      expect(transaction.context.summaries.en.title).toBe('EAS');
+      expect(contextSummary(transaction.context)).toBe(
+        '0xb2370e24dabd855bfcf87087740ca6bdb77ebd50 REVOKED offchain data',
       );
-      expect(transaction.context.variables).toMatchObject({
-        from: { value: '0xb2370e24dabd855bfcf87087740ca6bdb77ebd50' },
-      });
     });
   });
 
--- src/protocol/eas/schemaRegistry.spec.ts
+++ src/protocol/eas/schemaRegistry.spec.ts
@@ -1,5 +1,6 @@
 import { Transaction } from '../../types';
 import { detect, generate } from './schemaRegistry';
+import { contextSummary } from '../../helpers/utils';
 import schemaRegistryRegister0x87fc8922 from '../../test/transactions/eas-schema-registry-register-0x87fc8922.json';
 import catchall0xc35c01ac from '../../test/transactions/catchall-0xc35c01ac.json';
 
@@ -14,17 +15,11 @@ describe('SchemaRegistry', () => {
       const transaction = generate(
         schemaRegistryRegister0x87fc8922 as Transaction,
       );
-      expect(transaction.context.summaries.en.variables.registered?.type).toBe(
-        'contextAction',
-      );
+      expect(transaction.context.summaries.en.title).toBe('EAS');
 
-      expect(transaction.context.variables).toMatchObject({
-        from: { value: '0x4fa65b8e4d2018dff11c7aaf460548e413b844b1' },
-        id: {
-          value:
-            '0x5dcd6fd9212455e1d21d9236e0dfbdc8b35d37343a083d8f8d3c317352880ab5',
-        },
-      });
+      expect(contextSummary(transaction.context)).toBe(
+        '0x4fa65b8e4d2018dff11c7aaf460548e413b844b1 REGISTERED new schema with id 0x5dcd6fd9212455e1d21d9236e0dfbdc8b35d37343a083d8f8d3c317352880ab5'
+      );
     });
   });
 
