5867ddebddcac84d74aca28715a4113c22057514
ponyjackal <pony1125@outlook.com>
2023-12-13 08:57:38 -0800

    feat: add _schemaRegistryContextualizer

--- src/protocol/eas/index.ts
+++ src/protocol/eas/index.ts
@@ -1,8 +1,10 @@
 import { makeContextualize } from '../../helpers/utils';
 import { contextualize as _easContextualizer } from './eas';
+import { contextualize as _schemaRegistryContextualizer } from './schemaRegistry';
 
 const children = {
   _easContextualizer,
+  _schemaRegistryContextualizer,
 };
 
 const contextualize = makeContextualize(children);
