db4295267ada0053d19f0725a6064c566ac3b873
Daniel Öhrlund <danielohrlund@gmail.com>
2023-12-18 07:34:40 +0100

    define cryptopunk ABIs object using address constants as keys

--- src/protocol/cryptopunks/constants.ts
+++ src/protocol/cryptopunks/constants.ts
@@ -7,6 +7,6 @@ export const CryptopunksContracts = {
 } as const;
 
 export const CRYPTOPUNK_ABIS = {
-  '0x6ba6f2207e343923ba692e5cae646fb0f566db8d': cryptoPunksOldAbi,
-  '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb': cryptoPunksAbi,
+  [CryptopunksContracts.Old]: cryptoPunksOldAbi,
+  [CryptopunksContracts.New]: cryptoPunksAbi,
 };
