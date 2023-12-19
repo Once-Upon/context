3971925c4a4dd81e9c1c08e1de6a1cc82fd85fbe
ponyjackal <pony1125@outlook.com>
2023-12-19 05:16:37 -0800

    feat: add unit test

--- src/heuristics/erc20Mint/erc20Mint.spec.ts
+++ src/heuristics/erc20Mint/erc20Mint.spec.ts
@@ -1,10 +1,14 @@
 import { Transaction } from '../../types';
 import { detect } from './erc20Mint';
 import erc20Swap0xd55dc9b2 from '../../test/transactions/erc20Swap-0xd55dc9b2.json';
+import erc20Mint0x5bc8b9a8 from '../../test/transactions/erc20Mint-0x5bc8b9a8.json';
 import catchall0xc35c01ac from '../../test/transactions/catchall-0xc35c01ac.json';
 
 describe('ERC20 Mint', () => {
-  it('Should detect ERC20 Mint transaction', () => {});
+  it('Should detect ERC20 Mint transaction', () => {
+    const isTokenMint1 = detect(erc20Mint0x5bc8b9a8 as Transaction);
+    expect(isTokenMint1).toBe(true);
+  });
 
   it('Should not detect as ERC20 Mint', () => {
     const isTokenMint1 = detect(erc20Swap0xd55dc9b2 as Transaction);
