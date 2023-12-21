37298be00c78a666b7323254ed497b229f428ea9
ponyjackal <pony1125@outlook.com>
2023-12-21 09:58:54 -0800

    feat: add integration test for friendtech

--- src/contextualizer.spec.ts
+++ src/contextualizer.spec.ts
@@ -32,6 +32,11 @@ import erc1155Mint0x45d1ed7b from './test/transactions/erc1155Mint-0x45d1ed7b.js
 import erc721Mint0x35f54999 from './test/transactions/erc721Mint-0x35f54999.json';
 // token transfer
 import tokenTransfer0xcfba5dee from './test/transactions/tokenTransfer-0xcfba5dee.json';
+// FriendTech
+import friendTech0xde5ce243 from './test/transactions/friendTech-0xde5ce243.json';
+import friendTech0xe65b4bd6 from './test/transactions/friendTech-0xe65b4bd6.json';
+import friendTech0xed2dd79e from './test/transactions/friendTech-0xed2dd79e.json';
+import friendTech0x703647d1 from './test/transactions/friendTech-0x703647d1.json';
 
 const contextualize = makeContextualize({
   protocolContextualizer: protocolContextualizer.contextualize,
@@ -41,7 +46,7 @@ const contextualize = makeContextualize({
 describe('ContextualizerService', () => {
   describe('Detect transactions correctly', () => {
     it('Should detect Contract Deployment', async () => {
-      const contractDeployed1 = await contextualize(
+      const contractDeployed1 = contextualize(
         contractDeployed0x88e7d866 as Transaction,
       );
       expect(contractDeployed1.context.summaries.en.title).toBe(
@@ -50,108 +55,110 @@ describe('ContextualizerService', () => {
     });
 
     it('Should detect ERC20 Swap', async () => {
-      const erc20Swap1 = await contextualize(
-        erc20Swap0x8cb66698 as Transaction,
-      );
+      const erc20Swap1 = contextualize(erc20Swap0x8cb66698 as Transaction);
       expect(erc20Swap1.context.summaries.en.title).toBe('ERC20 Swap');
 
-      const erc20Swap2 = await contextualize(
-        erc20Swap0xd55dc9b2 as Transaction,
-      );
+      const erc20Swap2 = contextualize(erc20Swap0xd55dc9b2 as Transaction);
       expect(erc20Swap2.context.summaries.en.title).toBe('ERC20 Swap');
     });
 
     it('Should detect ERC721 Purchase', async () => {
-      const erc721Purchase1 = await contextualize(
+      const erc721Purchase1 = contextualize(
         erc721Purchase0x2558f104 as Transaction,
       );
       expect(erc721Purchase1.context.summaries.en.title).toBe('NFT Purchase');
     });
 
     it('Should detect ERC721 Sale', async () => {
-      const erc721Sale1 = await contextualize(
-        erc721Sale0x05b8cee6 as Transaction,
-      );
+      const erc721Sale1 = contextualize(erc721Sale0x05b8cee6 as Transaction);
       expect(erc721Sale1.context.summaries.en.title).toBe('NFT Purchase'); // TODO; This should be NFT Sale, update with new version
     });
 
     it('Should detect ERC1155 Purchase', async () => {
-      const erc1155Purchase1 = await contextualize(
+      const erc1155Purchase1 = contextualize(
         erc1155Purchase0x156df9f7 as Transaction,
       );
       expect(erc1155Purchase1.context.summaries.en.title).toBe('NFT Purchase');
     });
 
     it('Should detect ERC1155 Sale', async () => {
-      const erc1155Sale1 = await contextualize(
-        erc1155Sale0x16b2334d as Transaction,
-      );
+      const erc1155Sale1 = contextualize(erc1155Sale0x16b2334d as Transaction);
       expect(erc1155Sale1.context.summaries.en.title).toBe('NFT Purchase'); // TODO; This should be NFT Sale, update with new version
     });
 
     it('Should detect IDM', async () => {
-      const idm1 = await contextualize(idm0xf07ff1ad as Transaction);
+      const idm1 = contextualize(idm0xf07ff1ad as Transaction);
       expect(idm1.context.summaries.en.title).toBe('Input Data Message');
     });
 
     it('Should detect Token Airdrop', async () => {
-      const tokenAirdrop1 = await contextualize(
+      const tokenAirdrop1 = contextualize(
         tokenAirdrop0x9559fbd9 as Transaction,
       );
       expect(tokenAirdrop1.context.summaries.en.title).toBe('Token Airdrop');
 
-      const tokenAirdrop2 = await contextualize(
+      const tokenAirdrop2 = contextualize(
         tokenAirdrop0xe2a9a20b as Transaction,
       );
       expect(tokenAirdrop2.context.summaries.en.title).toBe('Token Airdrop');
 
-      const tokenAirdrop3 = await contextualize(
+      const tokenAirdrop3 = contextualize(
         tokenAirdrop0xb312ecc2 as Transaction,
       );
       expect(tokenAirdrop3.context.summaries.en.title).toBe('Token Airdrop');
     });
 
     it('Should detect Token Approval', async () => {
-      const tokenApproval1 = await contextualize(
+      const tokenApproval1 = contextualize(
         tokenApproval0x567130ba as Transaction,
       );
       expect(tokenApproval1.context.summaries.en.title).toBe('Token Approval');
 
-      const tokenApproval2 = await contextualize(
+      const tokenApproval2 = contextualize(
         tokenApproval0x06f15d49 as Transaction,
       );
       expect(tokenApproval2.context.summaries.en.title).toBe('Token Approval');
 
-      const tokenApproval3 = await contextualize(
+      const tokenApproval3 = contextualize(
         tokenApproval0xa0c2a425 as Transaction,
       );
       expect(tokenApproval3.context.summaries.en.title).toBe('Token Approval');
     });
 
     it('Should detect ERC721 Mint', async () => {
-      const erc721Mint1 = await contextualize(
+      const erc721Mint1 = contextualize(
         erc721MintMint0x2c8a3ed1 as Transaction,
       );
       expect(erc721Mint1.context.summaries.en.title).toBe('NFT Mint');
 
-      const erc721Mint2 = await contextualize(
-        erc721Mint0x35f54999 as Transaction,
-      );
+      const erc721Mint2 = contextualize(erc721Mint0x35f54999 as Transaction);
       expect(erc721Mint2.context.summaries.en.title).toBe('NFT Mint');
     });
 
     it('Should detect ERC1155 Mint', async () => {
-      const erc1155Mint1 = await contextualize(
-        erc1155Mint0x45d1ed7b as Transaction,
-      );
+      const erc1155Mint1 = contextualize(erc1155Mint0x45d1ed7b as Transaction);
       expect(erc1155Mint1.context.summaries.en.title).toBe('NFT Mint');
     });
 
     it('Should detect Token Transfer', async () => {
-      const tokenTransfer1 = await contextualize(
+      const tokenTransfer1 = contextualize(
         tokenTransfer0xcfba5dee as Transaction,
       );
       expect(tokenTransfer1.context.summaries.en.title).toBe('Token Transfer');
+    });
+
+    it('Should detect FriendTech transaction', () => {
+      const friendTech1 = contextualize(friendTech0xde5ce243 as Transaction);
+      expect(friendTech1.context.summaries.en.title).toBe('friend.tech');
+
+      const friendTech2 = contextualize(friendTech0xe65b4bd6 as Transaction);
+      expect(friendTech2.context.summaries.en.title).toBe('friend.tech');
+
+      const friendTech3 = contextualize(friendTech0xed2dd79e as Transaction);
+      expect(friendTech3.context.summaries.en.title).toBe('friend.tech');
+
+      const friendTech4 = contextualize(friendTech0x703647d1 as Transaction);
+      expect(friendTech4.context.summaries.en.title).toBe('friend.tech');
     });
   });
 });
