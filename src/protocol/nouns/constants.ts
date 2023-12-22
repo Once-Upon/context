4535f49f30977a2d545aa64c22957fce96fc921c
Daniel Öhrlund <danielohrlund@gmail.com>
2023-12-22 22:38:56 +0100

    add NounsBuilder Auction contextualizer

--- src/protocol/nouns/constants.ts
+++ src/protocol/nouns/constants.ts
@@ -1,10 +1,22 @@
 import NounsAuctionHouse from './abis/NounsAuctionHouse';
+import IAuction from './abis/IAuction';
 
 export const NounsContracts = {
   AuctionHouse: '0x830bd73e4184cef73443c15111a1df14e495c706',
   NFT: '0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03',
 };
 
+// Mappings between Auction and ERC721 contracts for known NounsBuilder DAO instances
+export const NounsBuilderDAOMappings = {
+  '0x43790fe6bd46b210eb27f01306c1d3546aeb8c1b':
+    '0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60', // Purple
+  '0x658d3a1b6dabcfbaa8b75cc182bf33efefdc200d':
+    '0xdf9b7d26c8fc806b1ae6273684556761ff02d422', // Builder
+  '0xf4e5889919d6d8b78354e3671d8b96ffd1708e52':
+    '0x351ea1a718521f22718ae14f7d380ae345fad043', // BLVKHVND
+};
+
 export const ABIs = {
   NounsAuctionHouse,
+  IAuction,
 };
---
+++ src/protocol/nouns/nounsBuilderAuction.spec.ts
@@ -0,0 +1,101 @@
+import { Transaction } from '../../types';
+import { contextSummary } from '../../helpers/utils';
+import { detect, generate } from './nounsBuilderAuction';
+
+import easAttest0xfed2349f from '../../test/transactions/eas-attest-mainnet-0xfed2349f.json';
+import nounsAuctionHouseBid0x4efdef57 from '../../test/transactions/nouns-auction-house-bid-0x4efdef57.json';
+import nounsAuctionHouseSettleAndCreate0x354aea2d from '../../test/transactions/nouns-auction-house-settle-and-create-0x354aea2d.json';
+
+import nounsBuilderAuctionPurpleCreateBid0x638faccc from '../../test/transactions/nouns-builder-auction-purple-create-bid-0x638faccc.json';
+import nounsBuilderAuctionPurpleSettleAndCreate0xc35c01ac from '../../test/transactions/nouns-builder-auction-purple-settle-and-create-0xc35c01ac.json';
+
+import nounsBuilderAuctionUnknownCreateBid0x401da075 from '../../test/transactions/nouns-builder-auction-unknown-create-bid-0x401da075.json';
+import nounsBuilderAuctionUnknownSettleAndCreate0x4446c0b0 from '../../test/transactions/nouns-builder-auction-unknown-settle-and-create-0x4446c0b0.json';
+
+describe('NounsBuilderDAO Auction', () => {
+  describe('createBid', () => {
+    it('Should detect transaction', () => {
+      const unknown = detect(
+        nounsBuilderAuctionUnknownCreateBid0x401da075 as Transaction,
+      );
+      expect(unknown).toBe(true);
+
+      const purple = detect(
+        nounsBuilderAuctionPurpleCreateBid0x638faccc as Transaction,
+      );
+      expect(purple).toBe(true);
+    });
+
+    it('Should not detect', () => {
+      // NOTE: the catchall we normally use is a settleCurrentAndCreateNewAuction
+      // call for PurpleDAO
+      const nouns1 = detect(nounsAuctionHouseBid0x4efdef57 as Transaction);
+      expect(nouns1).toBe(false);
+
+      const other = detect(easAttest0xfed2349f as Transaction);
+      expect(other).toBe(false);
+    });
+
+    it('Should generate context', () => {
+      const unknown = generate(
+        nounsBuilderAuctionUnknownCreateBid0x401da075 as Transaction,
+      );
+      const unknownDesc = contextSummary(unknown.context);
+      expect(unknownDesc).toBe(
+        '0xad8717e33c719a9b407ad6b40d0be237845c2fbf BID 0.0001 ETH on token #5',
+      );
+
+      const purple = generate(
+        nounsBuilderAuctionPurpleCreateBid0x638faccc as Transaction,
+      );
+      const purpleDesc = contextSummary(purple.context);
+      expect(purpleDesc).toBe(
+        '0x74b78e98093f5b522a7ebdac3b994641ca7c2b20 BID 0.099 ETH on 0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60 #242',
+      );
+    });
+  });
+
+  describe('settleCurrentAndCreateNewAuction', () => {
+    it('Should detect transaction', () => {
+      const unknown = detect(
+        nounsBuilderAuctionUnknownSettleAndCreate0x4446c0b0 as Transaction,
+      );
+      expect(unknown).toBe(true);
+
+      const purple = detect(
+        nounsBuilderAuctionPurpleSettleAndCreate0xc35c01ac as Transaction,
+      );
+      expect(purple).toBe(true);
+    });
+
+    it('Should not detect', () => {
+      // NOTE: the catchall we normally use is a settleCurrentAndCreateNewAuction
+      // call for PurpleDAO
+      const nouns1 = detect(
+        nounsAuctionHouseSettleAndCreate0x354aea2d as Transaction,
+      );
+      expect(nouns1).toBe(false);
+
+      const other = detect(easAttest0xfed2349f as Transaction);
+      expect(other).toBe(false);
+    });
+
+    it('Should generate context', () => {
+      const unknown = generate(
+        nounsBuilderAuctionUnknownSettleAndCreate0x4446c0b0 as Transaction,
+      );
+      const unknownDesc = contextSummary(unknown.context);
+      expect(unknownDesc).toBe(
+        '0xad8717e33c719a9b407ad6b40d0be237845c2fbf SETTLED auction for token #4 won by 0x2767500a75D90D711b2Ac27b3a032a0dAa40e4B2',
+      );
+
+      const purple = generate(
+        nounsBuilderAuctionPurpleSettleAndCreate0xc35c01ac as Transaction,
+      );
+      const purpleDesc = contextSummary(purple.context);
+      expect(purpleDesc).toBe(
+        '0x74b78e98093f5b522a7ebdac3b994641ca7c2b20 SETTLED auction for 0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60 #242 won by 0x74B78e98093F5B522A7eBDAc3B994641cA7c2b20',
+      );
+    });
+  });
+});
