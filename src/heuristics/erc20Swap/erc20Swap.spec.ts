17705488b7512e8afa398e19fe7a1e00a4cfeaec
ponyjackal <pony1125@outlook.com>
2023-12-21 16:09:36 -0800

    feat: add unit test for erc20 swap

--- src/heuristics/erc20Swap/erc20Swap.spec.ts
+++ src/heuristics/erc20Swap/erc20Swap.spec.ts
@@ -1,9 +1,10 @@
 import { Transaction } from '../../types';
-import { detect } from './erc20Swap';
+import { detect, generate } from './erc20Swap';
 import erc20Swap0x8cb66698 from '../../test/transactions/erc20Swap-0x8cb66698.json';
 import erc20SwapNot0xb376ca2f from '../../test/transactions/erc20Swap-not-0xb376ca2f.json';
 import erc20Swap0xd55dc9b2 from '../../test/transactions/erc20Swap-0xd55dc9b2.json';
 import erc20Swap0x6ef80cce from '../../test/transactions/erc20Swap-0x6ef80cce.json';
+import erc20Swap0xfdad8f4c from '../../test/transactions/erc20Swap-0xfdad8f4c.json';
 import catchall0xc35c01ac from '../../test/transactions/catchall-0xc35c01ac.json';
 
 describe('ERC20 Swap', () => {
@@ -16,6 +17,14 @@ describe('ERC20 Swap', () => {
 
     const isERC20Swap2 = detect(erc20Swap0x6ef80cce as Transaction);
     expect(isERC20Swap2).toBe(true);
+
+    const isERC20Swap3 = detect(erc20Swap0xfdad8f4c as Transaction);
+    expect(isERC20Swap3).toBe(true);
+  });
+
+  it('should generate ERC20 Swap context', () => {
+    const erc20Swap3 = generate(erc20Swap0xfdad8f4c as Transaction);
+    expect(erc20Swap3.context?.summaries.en.title).toBe('ERC20 Swap');
   });
 
   it('Should not detect as ERC20 Swap transaction', () => {
