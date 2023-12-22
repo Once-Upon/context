856fb11ff00978c4448bf35ef1962ab09aeef676
ponyjackal <pony1125@outlook.com>
2023-12-21 16:16:01 -0800

    fix: erc20 swap

--- src/heuristics/erc20Swap/erc20Swap.spec.ts
+++ src/heuristics/erc20Swap/erc20Swap.spec.ts
@@ -6,6 +6,7 @@ import erc20Swap0xd55dc9b2 from '../../test/transactions/erc20Swap-0xd55dc9b2.js
 import erc20Swap0x6ef80cce from '../../test/transactions/erc20Swap-0x6ef80cce.json';
 import erc20Swap0xfdad8f4c from '../../test/transactions/erc20Swap-0xfdad8f4c.json';
 import catchall0xc35c01ac from '../../test/transactions/catchall-0xc35c01ac.json';
+import { contextSummary } from '../../helpers/utils';
 
 describe('ERC20 Swap', () => {
   it('Should detect ERC20 Swap transaction', () => {
@@ -25,6 +26,16 @@ describe('ERC20 Swap', () => {
   it('should generate ERC20 Swap context', () => {
     const erc20Swap3 = generate(erc20Swap0xfdad8f4c as Transaction);
     expect(erc20Swap3.context?.summaries.en.title).toBe('ERC20 Swap');
+    expect(erc20Swap3.context?.variables.swapFrom['token']).toBe(
+      '0xeaa63125dd63f10874f99cdbbb18410e7fc79dd3',
+    );
+    expect(erc20Swap3.context?.variables.swapTo['token']).toBe(
+      '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    );
+    const desc3 = contextSummary(erc20Swap3.context);
+    expect(desc3).toBe(
+      '0xd8da6bf26964af9d7eed9e03e53415d37aa96045 SWAPPED 6000000000000000000000000 0xeaa63125dd63f10874f99cdbbb18410e7fc79dd3 for 14437397836 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    );
   });
 
   it('Should not detect as ERC20 Swap transaction', () => {
