59398932850e75214ce89d56869a96cacd52020a
Daniel Öhrlund <danielohrlund@gmail.com>
2024-01-02 13:53:26 +0100

    refactor NounsBuilder DAO mappings to include name & governor contracts

--- src/protocol/nouns/constants.ts
+++ src/protocol/nouns/constants.ts
@@ -7,15 +7,33 @@ export const NounsContracts = {
   NFT: '0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03',
 };
 
-// Mappings between Auction and ERC721 contracts for known NounsBuilder DAO instances
-export const NounsBuilderDAOMappings = {
-  '0x43790fe6bd46b210eb27f01306c1d3546aeb8c1b':
-    '0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60', // Purple
-  '0x658d3a1b6dabcfbaa8b75cc182bf33efefdc200d':
-    '0xdf9b7d26c8fc806b1ae6273684556761ff02d422', // Builder
-  '0xf4e5889919d6d8b78354e3671d8b96ffd1708e52':
-    '0x351ea1a718521f22718ae14f7d380ae345fad043', // BLVKHVND
-};
+interface NounsBuilderInstance {
+  name: string;
+  auctionHouse: `0x${string}`;
+  nft: `0x${string}`;
+  governor: `0x${string}`;
+}
+
+export const NOUNS_BUILDER_INSTANCES = [
+  {
+    name: 'Purple',
+    auctionHouse: '0x43790fe6bd46b210eb27f01306c1d3546aeb8c1b',
+    nft: '0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60',
+    governor: '0xfb4a96541e1c70fc85ee512420eb0b05c542df57',
+  },
+  {
+    name: 'Builder',
+    auctionHouse: '0x658d3a1b6dabcfbaa8b75cc182bf33efefdc200d',
+    nft: '0xdf9b7d26c8fc806b1ae6273684556761ff02d422',
+    governor: '0xe3f8d5488c69d18abda42fca10c177d7c19e8b1a',
+  },
+  {
+    name: 'BLVKHVND',
+    auctionHouse: '0xf4e5889919d6d8b78354e3671d8b96ffd1708e52',
+    nft: '0x351ea1a718521f22718ae14f7d380ae345fad043',
+    governor: '0xa54ce3c884c68c01596853b25a3208acefda540e',
+  },
+];
 
 export const ABIs = {
   NounsAuctionHouse,
