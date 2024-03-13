87186e631255de832bf4be1b3e155bc544d802f4
ponyjackal <pony1125@outlook.com>
2024-03-13 08:49:24 -0700

    feat: add bridge zora energy

---
+++ src/bridges/zora/index.ts
@@ -0,0 +1,11 @@
+import { makeContextualize } from '../../helpers/utils';
+import { contextualize as source } from './source';
+import { contextualize as destination } from './destination';
+
+const children = { source, destination };
+
+const contextualize = makeContextualize(children);
+
+export const zoraContextualizer = {
+  contextualize,
+};
