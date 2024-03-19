91ff190724b7ce722b17ad951c6e1e6d95aa1c81
tuguu-2go <tuguu@basename.app>
2024-03-20 01:02:35 +0800

    Add contextualizers for Base Name Service

---
+++ src/protocol/bns/registrar.spec.ts
@@ -0,0 +1,25 @@
+import { Transaction } from '../../types';
+import { contextSummary } from '../../helpers/utils';
+import { detect, generate } from './registrar';
+import bnsRegistrar0x7f1eeb75 from '../../test/transactions/bns-registrar-0x7f1eeb75.json';
+import catchall0xc35c01ac from '../../test/transactions/catchall-0xc35c01ac.json';
+
+describe('BNS Registrar', () => {
+  it('Should detect bns registrar transaction', () => {
+    const bns1 = detect(bnsRegistrar0x7f1eeb75 as Transaction);
+    expect(bns1).toBe(true);
+  });
+
+  it('Should not detect as bns registrar transaction', () => {
+    const bns1 = detect(catchall0xc35c01ac as Transaction);
+    expect(bns1).toBe(false);
+  });
+
+  it('Should generate bns context', () => {
+    const bns1 = generate(bnsRegistrar0x7f1eeb75 as Transaction);
+    const desc1 = contextSummary(bns1.context);
+    expect(desc1).toBe(
+      '0xf20d84df17a8c56244e1a9d4844bef2386eece84 REGISTERED boger.base for 365 days',
+    );
+  });
+});
