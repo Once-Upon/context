9ac2ccf29d10f8c5c777a6aee14104687ba5707c
Paul Cowgill <pauldavidcowgill@gmail.com>
2024-03-19 15:57:36 -0500

    Add new value to tx type

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -114,6 +114,7 @@ export interface Transaction {
   decode?: TransactionDescription;
   netAssetTransfers?: NetAssetTransfers;
   receipt?: Receipt;
+  canCopy?: boolean;
   context?: TransactionContextType;
   logs?: Log[];
 }
