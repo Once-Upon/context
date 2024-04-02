77073a7809c3075c218de044add7ebcf070d8595
ponyjackal <pony1125@outlook.com>
2024-04-02 04:26:58 -0700

    feat: add uniswapV2Pair abi

--- src/contextualizers/protocol/uniswapV2/constants.ts
+++ src/contextualizers/protocol/uniswapV2/constants.ts
@@ -1,4 +1,5 @@
-import uniswapV2Abi from './abis/UniswapV2Router';
+import uniswapV2RouterAbi from './abis/UniswapV2Router';
+import uniswapV2PairAbi from './abis/UniswapV2Pair';
 
 export const UNISWAP_V2_ROUTERS = {
   7777777: '0xa00f34a632630efd15223b1968358ba4845beec7',
@@ -7,4 +8,5 @@ export const UNISWAP_V2_ROUTERS = {
 export const ENJOY_CONTRACT_ADDRESS =
   '0xa6b280b42cb0b7c4a4f789ec6ccc3a7609a1bc39';
 
-export const UNISWAP_V2_ROUTER_ABI = uniswapV2Abi;
+export const UNISWAP_V2_ROUTER_ABI = uniswapV2RouterAbi;
+export const UNISWAP_V2_PAIR_ABI = uniswapV2PairAbi;
