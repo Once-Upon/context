0f48c89150f41ea7c3d74d898f83ef24fa9a030b
Paul Cowgill <pauldavidcowgill@gmail.com>
2024-04-05 15:27:46 -0500

    Export enum for each protocol

--- src/types/contextAction/heuristicContextAction.ts
+++ src/types/contextAction/heuristicContextAction.ts
@@ -1,15 +1,34 @@
 export type HeuristicContextAction =
-  | 'BOUGHT'
-  | 'BRIDGED'
-  | 'DEPLOYED'
-  | 'MINTED'
-  | 'SWAPPED'
-  | 'SENT'
-  | 'RECEIVED'
-  | 'COMMITTED_TO'
-  | 'RECEIVED_AIRDROP'
-  | 'GAVE_ACCESS'
-  | 'REVOKED_ACCESS'
-  | 'INTERACTED_WITH'
-  | 'SENT_MESSAGE'
-  | 'CANCELED_A_PENDING_TRANSACTION';
+  | HeuristicContextActionEnum.BOUGHT
+  | HeuristicContextActionEnum.BRIDGED
+  | HeuristicContextActionEnum.DEPLOYED
+  | HeuristicContextActionEnum.MINTED
+  | HeuristicContextActionEnum.SWAPPED
+  | HeuristicContextActionEnum.SENT
+  | HeuristicContextActionEnum.RECEIVED
+  | HeuristicContextActionEnum.COMMITTED_TO
+  | HeuristicContextActionEnum.RECEIVED_AIRDROP
+  | HeuristicContextActionEnum.GAVE_ACCESS
+  | HeuristicContextActionEnum.REVOKED_ACCESS
+  | HeuristicContextActionEnum.INTERACTED_WITH
+  | HeuristicContextActionEnum.CALLED
+  | HeuristicContextActionEnum.SENT_MESSAGE
+  | HeuristicContextActionEnum.CANCELED_A_PENDING_TRANSACTION;
+
+export enum HeuristicContextActionEnum {
+  BOUGHT = 'BOUGHT',
+  BRIDGED = 'BRIDGED',
+  DEPLOYED = 'DEPLOYED',
+  MINTED = 'MINTED',
+  SWAPPED = 'SWAPPED',
+  SENT = 'SENT',
+  RECEIVED = 'RECEIVED',
+  COMMITTED_TO = 'COMMITTED_TO',
+  RECEIVED_AIRDROP = 'RECEIVED_AIRDROP',
+  GAVE_ACCESS = 'GAVE_ACCESS',
+  REVOKED_ACCESS = 'REVOKED_ACCESS',
+  INTERACTED_WITH = 'INTERACTED_WITH',
+  CALLED = 'CALLED',
+  SENT_MESSAGE = 'SENT_MESSAGE',
+  CANCELED_A_PENDING_TRANSACTION = 'CANCELED_A_PENDING_TRANSACTION',
+}
