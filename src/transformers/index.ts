b3d343c2be2b2fd502570c278fc3138a265b0160
Daniel Öhrlund <danielohrlund@gmail.com>
2024-04-08 08:48:37 +0200

    improve how transformers are defined so that they are type checked
    accessing the transformer from children using children[key] returns a
    value of type any since key isn't a const so the type of children[key]
    cannot be known

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -34,7 +34,7 @@ const utils = {
 };
 
 const transformers = Object.fromEntries(
-  Object.keys(children).map((key) => [key, children[key].transform]),
+  Object.entries(children).map(([key, { transform }]) => [key, transform]),
 );
 
 const transform = makeTransform(transformers);
