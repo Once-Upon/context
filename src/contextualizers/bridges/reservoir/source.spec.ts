19cc33024503ca5a6360bfc896f59e586f14e473
ponyjackal <pony1125@outlook.com>
2024-05-07 12:34:10 -0700

    feat: add unit test for reservoir source

--- src/contextualizers/bridges/reservoir/source.spec.ts
+++ src/contextualizers/bridges/reservoir/source.spec.ts
@@ -4,6 +4,7 @@ import { containsBigInt, contextSummary } from '../../../helpers/utils';
 import bridgeZoraEnergySource0x72084c64 from '../../test/transactions/bridgeZoraEnergySource-0x72084c64.json';
 import zoraSource0xf2e656b3 from '../../test/transactions/zoraSource-0xf2e656b3.json';
 import hopSource0x8603ffab from '../../test/transactions/hop-source-0x8603ffab.json';
+import reservoirSource0x35c5ac27 from '../../test/transactions/reservoirSource-0x35c5ac27.json';
 
 describe('Bridge Zora Energy Source', () => {
   it('Should detect transaction', () => {
@@ -21,6 +22,11 @@ describe('Bridge Zora Energy Source', () => {
       zoraSource0xf2e656b3 as unknown as Transaction,
     );
     expect(isBridgeZoraEnergySource3).toBe(true);
+
+    const isBridgeZoraEnergySource4 = detect(
+      reservoirSource0x35c5ac27 as unknown as Transaction,
+    );
+    expect(isBridgeZoraEnergySource4).toBe(true);
   });
 
   it('Should generate context', () => {
@@ -41,5 +47,14 @@ describe('Bridge Zora Energy Source', () => {
       '0x17cd072cbd45031efc21da538c783e0ed3b25dcc INITIATED_A_CROSS_CHAIN_INTERACTION via 0xf70da97812cb96acdf810712aa562db8dfa3dbef and 3.829842894866116 ETH was transferred',
     );
     expect(containsBigInt(transaction2.context)).toBe(false);
+
+    const transaction3 = generate(
+      reservoirSource0x35c5ac27 as unknown as Transaction,
+    );
+    expect(transaction3.context?.summaries?.en.title).toBe('Bridge');
+    expect(contextSummary(transaction3.context)).toBe(
+      '0x74b78e98093f5b522a7ebdac3b994641ca7c2b20 INITIATED_A_CROSS_CHAIN_INTERACTION via 0xf70da97812cb96acdf810712aa562db8dfa3dbef and 0.201463549452958 ETH was transferred',
+    );
+    expect(containsBigInt(transaction3.context)).toBe(false);
   });
 });
