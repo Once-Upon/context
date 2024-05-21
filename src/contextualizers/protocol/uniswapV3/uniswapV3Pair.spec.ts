5eed15299591ae32dab3494564ad85327111e91b
ponyjackal <pony1125@outlook.com>
2024-05-21 03:47:50 -0700

    feat: add failing unit test for uniswapV3Pair

--- src/contextualizers/protocol/uniswapV3/uniswapV3Pair.spec.ts
+++ src/contextualizers/protocol/uniswapV3/uniswapV3Pair.spec.ts
@@ -3,6 +3,7 @@ import { detect, generate } from './uniswapV3Pair';
 import { containsBigInt, contextSummary } from '../../../helpers/utils';
 import uniswapV30xc5a57ee5 from '../../test/transactions/uniswapV3-0xc5a57ee5.json';
 import uniswapV3Pair0x6953c36b from '../../test/transactions/uniswapV3Pair-0x6953c36b.json';
+import uniswapV3Pair0xcc883774 from '../../test/transactions/uniswapV3Pair-0xcc883774.json';
 import catchall0xc35c01ac from '../../test/transactions/catchall-0xc35c01ac.json';
 
 describe('Uniswap', () => {
@@ -12,6 +13,9 @@ describe('Uniswap', () => {
 
     const swap2 = detect(uniswapV3Pair0x6953c36b as unknown as Transaction);
     expect(swap2).toBe(true);
+
+    const swap3 = detect(uniswapV3Pair0xcc883774 as unknown as Transaction);
+    expect(swap3).toBe(true);
   });
 
   it('Should generate context for uniswap v3 swap', () => {
@@ -32,6 +36,15 @@ describe('Uniswap', () => {
       '0x6f1cdbbb4d53d226cf4b917bf768b94acbab6168 swapped 56625778127422422604 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2 for 198805648076 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     );
     expect(containsBigInt(swap2.context)).toBe(false);
+
+    const swap3 = generate(uniswapV3Pair0xcc883774 as unknown as Transaction);
+    expect(swap3.context?.summaries?.category).toBe('PROTOCOL_1');
+    expect(swap3.context?.summaries?.en.title).toBe('Uniswap');
+    const desc3 = contextSummary(swap3.context);
+    expect(desc3).toBe(
+      '0xec8b0f7ffe3ae75d7ffab09429e3675bb63503e4 swapped 50000000000000000 0x4200000000000000000000000000000000000006 for 9670369113186014800347 0x4ed4e862860bed51a9570b96d89af5e1b0efefed',
+    );
+    expect(containsBigInt(swap3.context)).toBe(false);
   });
 
   it('Should not detect as UniswapV3', () => {
